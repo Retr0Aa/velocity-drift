08a46c8f826c67ac0355b12dba293e546197de7d
Alexander Buchkov <135352473+Retr0Aa@users.noreply.github.com>
2026-01-04 17:47:26 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,22 @@ import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const isMobile = /Android|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i.test(
+  navigator.userAgent
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    {isMobile ? (
+      <div style={{ textAlign: "center", marginTop: "0", fontSize: "24px" }}>
+        Velocity Drift is not supported on mobile devices.
+      </div>
+    ) : (
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    )}
   </React.StrictMode>
 );
